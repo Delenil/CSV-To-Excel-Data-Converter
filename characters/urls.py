05ee2d184f425ddb5a82e16af333ec1cc37f0db5
Vasko <vaskopoh@gmail.com>
2024-12-04 07:52:11 +0200

    Implementation of added functionality of data addition and removal.

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import upload_csv, character_list, export_to_excel
+from .views import upload_csv, character_list, export_to_excel, add_character, delete_character
 
 urlpatterns = [
     path('upload_csv/', upload_csv, name='upload_csv'),
     path('characters/', character_list, name='character_list'),
     path('export_to_excel/', export_to_excel, name='export_to_excel'),
+    path('add_character/', add_character, name='add_character'),
+    path('delete_character/<int:character_id>/', delete_character, name='delete_character'),
 ]
+
 
--- characters/views.py
+++ characters/views.py
@@ -1,9 +1,29 @@
-import csv
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Character
-from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 import pandas as pd
+from django.core.files.storage import FileSystemStorage
+
+MAX_TANKS = 2
+MAX_HEALS = 2
+MAX_DISPLAYED_CHARACTERS = 10
+
+# Allowed position-class mappings
+POSITION_CLASS_MAP = {
+    'Tank': ['Paladin', 'Warrior'],
+    'Heal': ['Shaman', 'Paladin', 'Druid'],
+    'Ranged_Dps': ['Warlock', 'Mage', 'Shaman'],
+    'Melee_Dps': ['Warrior', 'Paladin', 'Druid'],
+}
+
+
+def character_is_valid(name, character_class, position):
+    if position not in POSITION_CLASS_MAP:
+        return False
+    if character_class not in POSITION_CLASS_MAP[position]:
+        return False
+    return True
+
 
 def upload_csv(request):
     if request.method == "POST" and request.FILES['file']:
@@ -12,43 +32,122 @@ def upload_csv(request):
         filename = fs.save(csv_file.name, csv_file)
         filepath = fs.url(filename)
 
+        # Initialize counts and names set
+        tank_count = 0
+        heal_count = 0
+        names_set = set()
+        error_messages = []
 
-        with open(filepath[1:], mode='r') as file:
-
+        # Read CSV and save to database
+        with open(filepath[1:], mode='r') as file:  # Remove leading '/'
             character_data = {}
             for line in file:
-
                 line = line.strip()
                 if ':' in line:
                     key, value = map(str.strip, line.split(':', 1))
                     character_data[key] = value
 
-
                 if line == '':
+                    if character_data.get('Name') in names_set:
+                        error_messages.append(f"Names cannot be the same: {character_data['Name']}")
+                    else:
+                        names_set.add(character_data['Name'])
+
+                    if 'Tank' in character_data.get('Position', ''):
+                        tank_count += 1
+                    if 'Heal' in character_data.get('Position', ''):
+                        heal_count += 1
+
+                    # Validate character class and position
+                    if not character_is_valid(character_data.get('Name'), character_data.get('Class'),
+                                              character_data.get('Position')):
+                        error_messages.append(f"{character_data['Class']} cannot be a {character_data['Position']}.")
 
                     if all(k in character_data for k in ['Name', 'Class', 'Position']):
-                        Character.objects.create(
-                            name=character_data['Name'],
-                            character_class=character_data['Class'],
-                            position=character_data['Position']
-                        )
+                        if tank_count > MAX_TANKS:
+                            error_messages.append("There cannot be more than 2 Tanks.")
+                        if heal_count > MAX_HEALS:
+                            error_messages.append("There cannot be more than 2 Healers.")
+
+                        if not any(error_messages):
+                            Character.objects.create(
+                                name=character_data['Name'],
+                                character_class=character_data['Class'],
+                                position=character_data['Position']
+                            )
 
                     character_data = {}
 
-
             if character_data and all(k in character_data for k in ['Name', 'Class', 'Position']):
-                Character.objects.create(
-                    name=character_data['Name'],
-                    character_class=character_data['Class'],
-                    position=character_data['Position']
-                )
+                if character_data['Name'] in names_set:
+                    error_messages.append(f"Names cannot be the same: {character_data['Name']}")
+                else:
+                    names_set.add(character_data['Name'])
+
+                if 'Tank' in character_data.get('Position', ''):
+                    tank_count += 1
+                if 'Heal' in character_data.get('Position', ''):
+                    heal_count += 1
+
+                # Validate character class and position
+                if not character_is_valid(character_data.get('Name'), character_data.get('Class'),
+                                          character_data.get('Position')):
+                    error_messages.append(f"{character_data['Class']} cannot be a {character_data['Position']}.")
+
+                if tank_count > MAX_TANKS:
+                    error_messages.append("There cannot be more than 2 Tanks.")
+                if heal_count > MAX_HEALS:
+                    error_messages.append("There cannot be more than 2 Healers.")
+
+                if not any(error_messages):
+                    Character.objects.create(
+                        name=character_data['Name'],
+                        character_class=character_data['Class'],
+                        position=character_data['Position']
+                    )
+
+        if error_messages:
+            return render(request, 'upload_csv.html', {'error_messages': error_messages})
 
         return redirect('character_list')
     return render(request, 'upload_csv.html')
 
+
 def character_list(request):
-    characters = Character.objects.all()
+    characters = Character.objects.all()[:MAX_DISPLAYED_CHARACTERS]  # Limit to MAX_DISPLAYED_CHARACTERS
     return render(request, 'character_list.html', {'characters': characters})
+
+
+def add_character(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        character_class = request.POST.get('class')
+        position = request.POST.get('position')
+
+        if not character_is_valid(name, character_class, position):
+            return render(request, 'add_character.html',
+                          {'error_message': f"{character_class} cannot be a {position}."})
+
+        if Character.objects.filter(name=name).exists():
+            return render(request, 'add_character.html', {'error_message': 'Names cannot be the same.'})
+
+        if position == 'Tank' and Character.objects.filter(position='Tank').count() >= MAX_TANKS:
+            return render(request, 'add_character.html', {'error_message': 'There cannot be more than 2 Tanks.'})
+
+        if position == 'Heal' and Character.objects.filter(position='Heal').count() >= MAX_HEALS:
+            return render(request, 'add_character.html', {'error_message': 'There cannot be more than 2 Healers.'})
+
+        Character.objects.create(name=name, character_class=character_class, position=position)
+        return redirect('character_list')
+
+    return render(request, 'add_character.html')
+
+
+def delete_character(request, character_id):
+    character = get_object_or_404(Character, id=character_id)
+    character.delete()
+    return redirect('character_list')
+
 
 def export_to_excel(request):
     characters = Character.objects.all().values()
@@ -59,5 +158,4 @@ def export_to_excel(request):
 
     df.to_excel(response, index=False)
     return response
-
 
